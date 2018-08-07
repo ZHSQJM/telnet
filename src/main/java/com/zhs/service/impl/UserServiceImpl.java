566615164c97b031e65e45ccadbc5cda583b856c
ZHSYQ <2534798858@qq.com>
2018-08-07 18:49:15 +0800

    用户的实现从

--- src/main/java/com/zhs/service/impl/UserServiceImpl.java
+++ src/main/java/com/zhs/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -37,8 +38,18 @@ public class UserServiceImpl extends BaseService<TelnetUser> implements UserServ
         if(userList.size()>0){
             return ResultDataUtil.success("用户名已被注册",user.getUsername());
         }
+        Example example1 = new Example(TelnetUser.class);
+        Example.Criteria criteria1 = example1.createCriteria();
+        criteria1.andEqualTo("phonenumber",user.getPhonenumber());
+        List<TelnetUser> userList1 =telnetUserMapper.selectByExample(example1);
+        if(userList1.size()>0){
+            return ResultDataUtil.success("手机号码已被注册",user.getPhonenumber());
+        }
         //注册的用户默认的角色是普通用户 角色id是3
         TelnetUserRole tur=new TelnetUserRole();
+        Date date=new Date();
+        user.setDisabeld("0");
+        user.setCreateTime(date);
         telnetUserMapper.insert(user);
         tur.setRid(3);
         tur.setUid(user.getId());
