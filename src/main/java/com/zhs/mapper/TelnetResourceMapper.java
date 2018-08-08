0a89e674d5d65d2542f66f07add47e2af8cfd450
ZHSYQ <2534798858@qq.com>
2018-08-08 18:55:41 +0800

    mapper

--- src/main/java/com/zhs/mapper/TelnetResourceMapper.java
+++ src/main/java/com/zhs/mapper/TelnetResourceMapper.java
@@ -3,5 +3,10 @@ package com.zhs.mapper;
 import com.zhs.base.BaseMapper;
 import com.zhs.pojo.TelnetResource;
 
+import java.util.List;
+
 public interface TelnetResourceMapper extends BaseMapper<TelnetResource> {
+
+
+    public List<TelnetResource> queryAll();
 }
--- src/main/java/com/zhs/mapper/TelnetRoleMapper.java
+++ src/main/java/com/zhs/mapper/TelnetRoleMapper.java
@@ -3,6 +3,11 @@ package com.zhs.mapper;
 import com.zhs.base.BaseMapper;
 import com.zhs.pojo.TelnetRole;
 
+import java.util.List;
+
 public interface TelnetRoleMapper extends BaseMapper<TelnetRole> {
 
+
+    //根据用户的id查找出用户的角色
+    List<TelnetRole> queryRoleListWithSelected(Integer id);
 }
