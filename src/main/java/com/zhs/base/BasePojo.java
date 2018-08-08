776ab330261128c18887383218916cf1056222ca
ZHSYQ <2534798858@qq.com>
2018-08-08 18:56:11 +0800

    mapper

---
+++ src/main/java/com/zhs/base/BasePojo.java
@@ -0,0 +1,12 @@
+package com.zhs.base;
+
+import java.io.Serializable;
+
+/**
+ * Created with IDEA
+ * author:zhouhuasheng
+ * Date:2018/8/816:53
+ * 描述:
+ **/
+public class BasePojo implements Serializable {
+}
