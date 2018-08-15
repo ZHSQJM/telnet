348e3bc619db941aacde6279cbd918460acaa5c8
zhs <zhs145612zhs>
2018-08-15 17:53:07 +0800

    lll

--- src/main/java/com/zhs/shiro/TelnetRealm.java
+++ src/main/java/com/zhs/shiro/TelnetRealm.java
@@ -1,49 +1,71 @@
 package com.zhs.shiro;
 
+import com.zhs.pojo.TelnetPermission;
 import com.zhs.pojo.TelnetUser;
-import com.zhs.service.ResourceService;
-import com.zhs.service.RoleService;
+import com.zhs.service.PermissionService;
 import com.zhs.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.mgt.RealmSecurityManager;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
 import org.apache.shiro.util.ByteSource;
+import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Resource;
+import java.util.*;
 
 /**
  * Created with IDEA
- * author:zhouhuasheng
- * Date:2018/8/89:26
- * 描述:
+ * author:周华生
+ * Date:2018/8/13 14:56
+ * 描述:自定义realm
  **/
 
-@Slf4j
-public class TelnetRealm extends AuthorizingRealm {
 
-    @Autowired
+public class TelnetRealm  extends AuthorizingRealm {
+
+    @Resource
     private UserService userService;
 
-    @Autowired
-    private RoleService roleService;
+    @Resource
+    private PermissionService resourcesService;
 
     @Autowired
-    private ResourceService resourceService;
+    private RedisSessionDAO redisSessionDAO;
 
+    //授权
     @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        return null;
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        TelnetUser user= (TelnetUser) SecurityUtils.getSubject().getPrincipal();//User{id=1, username='admin', password='3ef7164d1f6167cb9f2658c07d3c2f0a', enable=1}
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("userid",user.getId());
+        List<TelnetPermission> resourcesList = resourcesService.loadUserResources(map);
+        // 权限信息对象info,用来存放查出的用户的所有的角色（role）及权限（permission）
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        for(TelnetPermission resources: resourcesList){
+            info.addStringPermission(resources.getResurl());
+        }
+        return info;
     }
 
+    //认证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         //获取用户的输入的账号.
         String username = (String)token.getPrincipal();
-        TelnetUser user = userService.selectUserByName(username);
+        TelnetUser user = userService.selectByUsername(username);
         if(user==null) throw new UnknownAccountException();
+        if (0==user.getEnable()) {
+            throw new LockedAccountException(); // 帐号锁定
+        }
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
                 user, //用户
                 user.getPassword(), //密码
@@ -55,5 +77,42 @@ public class TelnetRealm extends AuthorizingRealm {
         session.setAttribute("userSession", user);
         session.setAttribute("userSessionId", user.getId());
         return authenticationInfo;
+    }
+
+
+    /**
+     * 根据userId 清除当前session存在的用户的权限缓存
+     * @param userIds 已经修改了权限的userId
+     */
+    public void clearUserAuthByUserId(List<Integer> userIds){
+        if(null == userIds || userIds.size() == 0)	return ;
+        //获取所有session
+        Collection<Session> sessions = redisSessionDAO.getActiveSessions();
+        //定义返回
+        List<SimplePrincipalCollection> list = new ArrayList<SimplePrincipalCollection>();
+        for (Session session:sessions){
+            //获取session登录信息。
+            Object obj = session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
+            if(null != obj && obj instanceof SimplePrincipalCollection){
+                //强转
+                SimplePrincipalCollection spc = (SimplePrincipalCollection)obj;
+                //判断用户，匹配用户ID。
+                obj = spc.getPrimaryPrincipal();
+                if(null != obj && obj instanceof TelnetUser){
+                    TelnetUser user = (TelnetUser) obj;
+                    System.out.println("user:"+user);
+                    //比较用户ID，符合即加入集合
+                    if(null != user && userIds.contains(user.getId())){
+                        list.add(spc);
+                    }
+                }
+            }
+        }
+        RealmSecurityManager securityManager =
+                (RealmSecurityManager) SecurityUtils.getSecurityManager();
+        TelnetRealm realm = (TelnetRealm)securityManager.getRealms().iterator().next();
+        for (SimplePrincipalCollection simplePrincipalCollection : list) {
+            realm.clearCachedAuthorizationInfo(simplePrincipalCollection);
+        }
     }
 }
