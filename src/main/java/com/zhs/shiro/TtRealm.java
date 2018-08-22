3d6893bac96f9b1936c488666b896fdf881bb24f
zhs <zhs145612zhs>
2018-08-22 15:57:56 +0800

    lll

---
+++ src/main/java/com/zhs/shiro/TtRealm.java
@@ -0,0 +1,67 @@
+package com.zhs.shiro;
+
+import com.zhs.pojo.TtPermission;
+import com.zhs.pojo.TtUser;
+import com.zhs.service.PermissionService;
+import com.zhs.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IDEA
+ * author:周华生
+ * Date:2018/8/17 14:42
+ * 描述://自定义的realm
+ **/
+@Slf4j
+public class TtRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PermissionService permissionService;
+
+    //执行授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("执行授权逻辑");
+
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        TtUser user= (TtUser) SecurityUtils.getSubject().getPrincipal();
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("userid",user.getId());
+
+       List<TtPermission> list= permissionService.loadAllPer(map);
+       for(TtPermission tp:list){
+           log.info("该用户的权限是"+tp.getPerms());
+           info.addStringPermission(tp.getPerms());
+       }
+        return info;
+    }
+
+    //执行认证逻辑
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+       System.out.println("执行认证逻辑");
+        UsernamePasswordToken  token=(UsernamePasswordToken)authenticationToken;
+       String name= token.getUsername();
+        TtUser user=userService.findUserByUserName(name);
+        if(user==null){
+            log.info("们没有该用户");
+             return  null; //shiro会抛出异常unkonow
+        }
+        //password一定时数据库的密码
+        return new SimpleAuthenticationInfo(user,user.getPassword(),"");
+    }
+}
