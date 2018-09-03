c5290a0481a3bf55861cec2bf650aa411b297eb7
ZHSYQ <2534798858@qq.com>
2018-09-03 09:35:53 +0800

    1

--- src/main/java/com/zhs/TelnetApplication.java
+++ src/main/java/com/zhs/TelnetApplication.java
@@ -2,10 +2,13 @@ package com.zhs;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.zhs.mapper")
+/*@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})*/
 public class TelnetApplication {
 
     public static void main(String[] args) {
